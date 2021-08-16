a4af1b830da1d6512eb148a43c30c209a03941a8
Abhishek Singh <Abhishek_110388@yahoo.com>
2021-08-16 21:34:05 +0530

    Inital_Project_Created

---
+++ Example/Pods/Target Support Files/Pods-SampleSDK_UMA_Example/Pods-SampleSDK_UMA_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleSDK_UMA_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SampleSDK_UMA_Example
+@end
---
+++ Example/Pods/Target Support Files/SampleSDK_UMA/SampleSDK_UMA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SampleSDK_UMA : NSObject
+@end
+@implementation PodsDummy_SampleSDK_UMA
+@end
